a1a2529f4282043609cf47b86406efc2bb69bf6b
Arissa Fram <arissafram@gmail.com>
2017-12-09 14:48:37 -0500

    fix sessions_controller bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -33,6 +33,6 @@ class SessionsController < ApplicationController
   private
 
   def user_params
-    params.require(:user. permit(:username, :password)
+    params.require(:user).permit(:username, :password)
   end
 end
