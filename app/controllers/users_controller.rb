ce00ec78e2292a9b2505c37777085333c07350b4
Arissa Fram <arissafram@gmail.com>
2017-12-09 14:20:07 -0500

    add user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(create_params)
+
+    if @user.save)
+      sign_in(@user)
+      flash[:notice] = 'You are signed in!'
+      redirect_to users_path
+    else
+      flash[:error] = @user.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def create_params
+    params.require(:user).permit(:username, :password)
+  end
+end
